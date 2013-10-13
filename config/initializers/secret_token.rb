9ec8867339337316c216604e3fd9e9dc54c2c4b0
Carlos Rodríguez <carlosr706@gmail.com>
2013-10-13 18:19:47 -0430

    Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trino::Application.config.secret_token = 'cf078c3709753fd20b40cf6c2c2c2ad85833fc5be41e67086bf66eb3f54e123bc3f3b62912c97ac50dd9e2c6042e434c2081d9c0ccdeda9869f4048d3605a079'
