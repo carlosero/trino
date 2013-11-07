61aec0de40b8d3d00399b87b338e60f6d076fe0a
Carlos Rodríguez <carlosr706@gmail.com>
2013-11-06 23:46:55 -0430

    Add datetimepicker and change some labels to rails standards

--- app/models/client.rb
+++ app/models/client.rb
@@ -14,19 +14,27 @@ class Client < ActiveRecord::Base
   }
 
   STATUS_FOR_SELECT = [
-    [I18n.t("models.clients.statuses.closed"),     'closed'    ],
-    [I18n.t("models.clients.statuses.waiting"),    'waiting'   ],
-    [I18n.t("models.clients.statuses.no_contact"), 'no_contact'],
-    [I18n.t("models.clients.statuses.negotiate"),  'negotiate' ],
-    [I18n.t("models.clients.statuses.contacted"),  'contacted' ],
+    [I18n.t("activerecord.attributes.client.statuses.closed"),     'closed'    ],
+    [I18n.t("activerecord.attributes.client.statuses.waiting"),    'waiting'   ],
+    [I18n.t("activerecord.attributes.client.statuses.no_contact"), 'no_contact'],
+    [I18n.t("activerecord.attributes.client.statuses.negotiate"),  'negotiate' ],
+    [I18n.t("activerecord.attributes.client.statuses.contacted"),  'contacted' ],
   ]
 
   MARITAL_STATUS = [
-    [I18n.t("models.clients.marital_statuses.single"),     'single'    ],
-    [I18n.t("models.clients.marital_statuses.married"),    'married'   ],
-    [I18n.t("models.clients.marital_statuses.divorced"),    'divorced'   ],
-    [I18n.t("models.clients.marital_statuses.widowed"),    'widowed'   ],
+    [I18n.t("activerecord.attributes.client.marital_statuses.single"),     'single'    ],
+    [I18n.t("activerecord.attributes.client.marital_statuses.married"),    'married'   ],
+    [I18n.t("activerecord.attributes.client.marital_statuses.divorced"),    'divorced'   ],
+    [I18n.t("activerecord.attributes.client.marital_statuses.widowed"),    'widowed'   ],
   ]
+
+  def agenda
+    read_attribute(:agenda).strftime('%d-%m-%Y %I:%M %P') if read_attribute(:agenda)
+  end
+
+  def expiration_date
+    read_attribute(:expiration_date).strftime('%d-%m-%Y %I:%M %P') if read_attribute(:expiration_date)
+  end
 
 
 end
